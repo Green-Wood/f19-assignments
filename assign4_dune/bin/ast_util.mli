96fa9b02537a42ce5ae1fe566934ad1e1fe29938
Green-Wood <1027572886a@gmail.com>
2023-10-06 16:19:21 +0800

    init for assign4_dune

---
+++ assign4_dune/bin/ast_util.mli
@@ -0,0 +1,15 @@
+module Type : sig
+  open Ast.Type
+
+  val to_debruijn : t -> t
+  val aequiv : t -> t -> bool
+  val substitute : string -> t -> t -> t
+end
+
+module Expr : sig
+  open Ast.Expr
+
+  val to_debruijn : t -> t
+  val aequiv : t -> t -> bool
+  val substitute : string -> t -> t -> t
+end
---
+++ assign4_dune/bin/flags.mli
@@ -0,0 +1,6 @@
+val set_verbose : bool -> unit
+val verbose : unit -> bool
+val set_extra_verbose : bool -> unit
+val extra_verbose : unit -> bool
+val set_testing : bool -> unit
+val testing : unit -> bool
